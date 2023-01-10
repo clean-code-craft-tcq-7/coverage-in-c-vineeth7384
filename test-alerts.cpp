fdae47ccc73a239893b040d387dec6796018c668
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-10 11:51:26 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,6 +6,27 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+TEST_CASE("infers the breach according to High") {
+  REQUIRE(inferBreach(30, 10, 20) == TOO_LOW);
+}
+TEST_CASE("infers the breach according to Normal") {
+  REQUIRE(inferBreach(20, 10, 40) == Normal);
+}
+TEST_CASE(" SendAlert messages to controller in high state") {
+  REQUIRE(SendAlertMessage(TO_CONTROLLER, TOO_HIGH);
+}
+TEST_CASE(" SendAlert messages to controller in low state") {
+  REQUIRE(SendAlertMessage(TO_CONTROLLER, TOO_LOW);
+}
+TEST_CASE(" SendAlert messages to email in high state") {
+  REQUIRE(SendAlertMessage(TO_EMAIL, TOO_HIGH);
+}
+TEST_CASE(" SendAlert messages to controller in low state") {
+  REQUIRE(SendAlertMessage(TO_EMAIL, TOO_LOW);
+}
 
 
 
