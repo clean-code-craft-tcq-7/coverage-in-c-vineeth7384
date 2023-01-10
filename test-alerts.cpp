4d7a1ba89267296bfc1b94b2e637ea7d52324bcc
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-10 19:29:06 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -24,15 +24,14 @@ TEST_CASE("classifyTemperatureBreach acive cooling cooling") {
 TEST_CASE("classifyTemperatureBreach medactive cooling") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30) == NORMAL);
 }
-TEST_CASE("classifyTemperatureBreach passive cooling Too LOW") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == TOO_LOW);
+TEST_CASE("classifyTemperatureBreach passive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 50) == TOO_HIGH);
 }
-TEST_CASE("classifyTemperatureBreach acive cooling cooling Too LOW") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == TOO_LOW);
+TEST_CASE("classifyTemperatureBreach acive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == TOO_HIGH);
 }
-TEST_CASE("classifyTemperatureBreach medactive cooling Too LOW") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == TOO_LOW);
+TEST_CASE("classifyTemperatureBreach medactive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 60) == TOO_HIGH);
 }
-
 
 
