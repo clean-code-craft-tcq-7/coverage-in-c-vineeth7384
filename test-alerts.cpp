580e68a1da3ac6652fbabe52442c3ad2600e2c35
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-10 15:14:55 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,9 +8,6 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
-TEST_CASE("infers the breach according to High") {
-  REQUIRE(inferBreach(30, 10, 20) == TOO_LOW);
-}
 TEST_CASE("infers the breach according to Normal") {
   REQUIRE(inferBreach(20, 10, 40) == NORMAL);
 }
